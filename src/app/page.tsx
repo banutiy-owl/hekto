ef3533cb1e1794a0b1eb449423f2e55a30dcc4ee
Kateryna Kononova <kkononova@griddynamics.com>
2025-03-09 19:01:27 +0100

    header

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import '../styles/css/style.css';
+import Header from "./components/Header";
+
+export default function Home() {
+  return (
+    <div >
+      <Header/>
+      <main >
+       
+      </main>
+     
+    </div>
+  );
+}
