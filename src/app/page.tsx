c7e8b698f7616544a53f6a450ee9f4a70efa8734
Kateryna Kononova <kkononova@griddynamics.com>
2025-03-12 00:03:39 +0100

    hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,42 @@
-import '../styles/css/style.css';
+import "../styles/css/style.css";
 import Header from "./components/Header";
 
+import Carousel from "./components/Carousel";
+
 export default function Home() {
+  const items = [
+    {
+      imageSmall: "lamp.png",
+      imageMain: "main-image.png",
+      title: "New Trendy Collection Headphones",
+      subtitle: "Best Headphones For Your Life....",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscin in phasellus non in justo.",
+    },
+    {
+      imageSmall: "lamp.png",
+      imageMain: "main-image.png",
+      title: "New Trendy Collection Headphones",
+      subtitle: "Best Headphones For Your Life....",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscin in phasellus non in justo.",
+    },
+    {
+      imageSmall: "lamp.png",
+      imageMain: "main-image.png",
+      title: "New Trendy Collection Headphones",
+      subtitle: "Best Headphones For Your Life....",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscin in phasellus non in justo.",
+    },
+  ];
+
   return (
-    <div >
-      <Header/>
-      <main >
-       
+    <div>
+      <Header />
+      <main>
+        <Carousel items={items} />
       </main>
-     
     </div>
   );
 }
